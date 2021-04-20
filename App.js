df5f67e2e8d9315cab17f6bb902dd8093f7a7cfe
ArtyomLyakhov <artyom.lyakhov.dev@gmail.com>
2021-04-20 13:37:41 +0300

    MainScreen

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import FirstApp from "./src/FirstApp.js";
+
+export default class App extends Component {
+  render(){
+    return <FirstApp />;
+  }
+}
